8180d7601e284c6b4c2b36f24e6bfd18341d4d26
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-16 04:56:29 +0000

    Automatically backed up by Learn

---
+++ js/challenge.js
@@ -0,0 +1,41 @@
+let counter=0;
+let count=document.getElementById("counter");
+let minus=document.getElementById("minus");
+let plus=document.getElementById("plus");
+let heart=document.getElementById("heart");
+let pause=document.getElementById("pause");
+let list=document.getElementById("list");
+let submit=document.getElementById("submit");
+let commentInput=document.getElementById("comment-input");
+let set=window.setInterval(myCallback, 1000);
+function myCallback(){
+  counter++
+  count.innerHTML=counter;
+}
+minus.addEventListener("click",function(){
+  --counter;
+  count.innerHTML=counter;
+})
+plus.addEventListener("click",function(){
+  ++counter;
+  count.innerHTML=counter;
+})
+
+
+pause.addEventListener("click",function(){
+  if(pause.innerHTML==="pause"){
+    window.clearInterval(set);
+    pause.innerHTML="continue";}
+    else{
+      set=window.setInterval(myCallback, 1000);
+      pause.innerHTML="pause";
+    }
+})
+
+submit.addEventListener("click",function(event){
+  let text = document.createTextNode(commentInput.value);
+  let p = document.createElement("p");
+  p.appendChild(text);
+  list.appendChild(p);
+  event.preventDefault()
+})
